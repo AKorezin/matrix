8ca403c9a396cc18e6fe630793342340e33c7e98
AKorezin <akorezin@localhost>
2018-10-22 18:26:52 +0300

    License added

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "Matrix.h"
 int main(int argc, char **argv){
-    Matrix *m = new Matrix(4);
+    Matrix *m = new Matrix(3);
     m->init();
     m->setElement(0,0,1);
     std::cout << m->getElement(0,0) << std::endl;
     m->rot();
-    std::cout << m->getElement(4,0) << std::endl;
+    std::cout << m->getElement(3,0) << std::endl;
     m->rot();
-    std::cout << m->getElement(4,4) << std::endl;
+    std::cout << m->getElement(3,3) << std::endl;
     m->rot();
-    std::cout << m->getElement(0,4) << std::endl;
+    std::cout << m->getElement(0,3) << std::endl;
     return 0;
 }
